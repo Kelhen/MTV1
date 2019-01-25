bf9feae02ba1b261058baf38b86202f65b692614
Kelhen <Kelhen@users.noreply.github.com>
2019-01-25 10:04:02 +0900

    prop exemple

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Meetup Tokyo Vue.js #1 - Let's get started!"/>
   </div>
 </template>
 
---
+++ src/views/Prop.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Tweet v-for="tweet in tweets" :key="tweet" :msg="tweet"/>
+  </div>
+</template>
+
+
+<script>
+import Tweet from '../components/Tweet.vue'
+
+export default {
+  name: 'Prop',
+  components: {
+    Tweet,
+  },
+  data() {
+    return {
+      tweets: ['Hello word', 'my second tweet'],
+    };
+  }
+}
+</script>
+
+<style scoped>
+</style>
