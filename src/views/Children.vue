10c307b83ee259c9679a9fa5ba5d9c32e8750a15
Kelhen <Kelhen@users.noreply.github.com>
2019-02-03 14:56:21 +0900

    add children exemple

---
+++ src/views/Children.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Tweet v-for="tweet in tweets" :key="tweet" :msg="tweet"/>
+    <button @click="addTweet()"> add tweet </button>
+    <button @click="countTweets">count childs</button>
+    <p>number of tweets : {{ childs }}</p>
+  </div>
+</template>
+
+
+<script>
+import Tweet from '../components/Tweet.vue'
+
+export default {
+  name: 'Prop',
+  components: {
+    Tweet,
+  },
+  data() {
+    return {
+      tweets: ['Hello word', 'my second tweet'],
+      childs: 0,
+    };
+  },
+  methods: {
+    countTweets() {
+      this.childs = this.$children.length;
+    },
+    addTweet () {
+      this.tweets.push('eee');
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
