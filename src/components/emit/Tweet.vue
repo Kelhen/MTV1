01e8d505fa00c8901b3fb0d18fbd2931d55ef6d3
Kelhen <Kelhen@users.noreply.github.com>
2019-02-03 14:56:37 +0900

    add emit exemple

---
+++ src/components/emit/Tweet.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>test: {{ msg }} <button @click="deleteTweet()">delete</button></div>
+</template>
+
+<script>
+export default {
+  name: 'Tweet',
+  props: {
+    msg: String
+  },
+  methods: {
+    deleteTweet() {
+      this.$emit('delete');
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Emit.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Tweet v-for="(tweet, index) in tweets" :key="index" :msg="tweet" @delete="deleteTweet(index)"/>
+  </div>
+</template>
+
+
+<script>
+import Tweet from '../components/emit/Tweet.vue'
+
+export default {
+  name: 'Prop',
+  components: {
+    Tweet,
+  },
+  data() {
+    return {
+      tweets: ['Hello word', 'my second tweet'],
+    };
+  },
+  methods : {
+    deleteTweet(index) {
+      this.$delete(this.tweets, index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
